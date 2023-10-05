09dbe53e5f4922ea49cee4916147e7ae5d77ede9
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-10-05 15:51:40 +0700

    gatau

--- .data/config.php
+++ .data/config.php
@@ -1,4 +1,16 @@
 <?php $configs = '
 {
+    "admin": "admin123",
+    "background": "aHR0cHM6Ly90ZWxlZ3JhLnBoL2ZpbGUvZGZlZDgyMmJiNDkxMjVmYzMzNmMyLmpwZw",
+    "passfile": "cHdkLnR4dA",
+    "theme": "renexmoe-ryzen-edition.html",
+    "timezone": "7",
+    "disktag": "onedrive_node1",
+    "onedrive_node1": {
+        "Driver": "Onedrive",
+        "diskname": "Unl6ZW5DbG91ZF9Ob2RlMQ",
+        "refresh_token": "0.Ab0A7ArypGFrmECY4459mBks2ij5TnNM11VFjRvZQvoKGkHLAKA.AgABAAEAAAAtyolDObpQQ5VtlI4uGjEPAgDs_wUA9P_GxO4acIbY9lcIG8aJ7rOIVt9CEPUSk2_BqS6Jtb0ry_qAxxRvOOzaum392pEdQcgqF2tFChVWWeLEkMqkojLZbAL92QHcdiMycPsWVzfTY9oseOm6_BElwVpaesiEdCHPXSr2nU5sjHI50afTkgBlNNB9VlqZnL6ft7rlIVj_JOzzNslHzzUlfLhaZMZaeIj05eTUwf_yndR2Z3cKDzRxOiDvqRdRA0yoiGl9Q6XDFKTQEVT6ngXFemmCLi4e0oWhhOX8ic-gYzut-mc1jc9w08OLoWfXDnAIT0ouphoFTZWE24M3A-4HGOACJ9_ztD_yDDezVImTM829mj38CLHLRYuijYGeWGGT7x_ry1LNW38S6yzgtxKDmySx94ffQkkPmwP8CtYqOAn3ZUOFsC2fjtjtcLimpMMpMzjVsrdxDva86TVSoYCH_m3xPRiwcNqSuzOIXFwHwoLGYsvy36d9fiAK10d-9KugxAOZBQ6zi4qFhjogQGbNfQfdabhjNYQcrDLtGKBcJw5zRK5QbvXSHx89NdoiKk6u31igqyBOKQVlFYAQ3lWs-YOfkztiJLmSXW1Ahz_-8Sorzj4Bz8YlWU1YMLweSozzKCvlTBsADcI0n7sPeQEL2uA2CwrROI5ERFL8z7mgdJimrNpByhQzjMxJ_Go1vi7F30t7LvqjiQG-Oi7L3jytqEp0OMogztU8s5kMU07sbZZ-ZevfplKF3tPDGmIPqcMB7XMOb-w8vwHhrOkqyc2xT-nHugELfZqN3Qya3a1rQ_2d3m2TRTs4KEZ9MxAm",
+        "token_expires": 1697099761
+    }
 }
 ';
